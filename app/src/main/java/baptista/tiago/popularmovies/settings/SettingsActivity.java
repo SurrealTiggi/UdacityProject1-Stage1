46ae8617f290fe2d6e977023488eb203a1fbab74
SurrealTiggi <surreal17T>
2015-09-23 21:10:01 +0200

    + Settings class

---
+++ app/src/main/java/baptista/tiago/popularmovies/settings/SettingsActivity.java
@@ -0,0 +1,51 @@
+package baptista.tiago.popularmovies.settings;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import baptista.tiago.popularmovies.R;
+
+public class SettingsActivity extends PreferenceActivity
+    implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = SettingsActivity.class.getName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
+
+        addPreferencesFromResource(R.xml.settings);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_sort_order_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_misc_key)));
+
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+}
